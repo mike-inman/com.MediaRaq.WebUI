45095584adf4c357211b5ac178a902bc3bf36216
Michael Inman <michael.d.inman@gmail.com>
2018-07-28 08:04:59 -0500

    Initial commit

---
+++ com.MediaRaq.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace com.MediaRaq.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
